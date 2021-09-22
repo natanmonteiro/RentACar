130f3744598cbde8f1a83a8f62201b9bfffbcd34
gabslock <84462423+gabslock@users.noreply.github.com>
2021-09-22 10:27:53 +0100

    v2
    Created the main classes

---
+++ src/algorithmsandconstructs/BookingSystem.java
@@ -0,0 +1,20 @@
+/*
+@name: Natan Souza and Gabriel Jucá
+@date: 8th Sep 2021
+BookingSystem.java
+ */
+package algorithmsandconstructs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class BookingSystem implements BookingSystemInterface{
+
+    @Override
+    public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
---
+++ src/algorithmsandconstructs/RentACar.java
@@ -0,0 +1,54 @@
+/*
+@name: Natan Souza and Gabriel Jucá
+@date: 8th Sep 2021
+RentACar.java
+ */
+package algorithmsandconstructs;
+
+import algorithmsandconstructs.enums.Make;
+import algorithmsandconstructs.enums.Month;
+import java.util.List;
+
+public class RentACar implements RentACarInterface{
+
+    @Override
+    public List<CarInterface> getCars() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCars(List<CarInterface> cars) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean checkAvailability(Month month, int day, Make make, int lengthOfRent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getCarAvailable(Month month, int day, Make make, int lengthOfRent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean bookCar(Month month, int day, Make make, int lengthOfRent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getNumberOfCars() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
