1547bc770786cedfb0443827b8e5ca3029c23c8c
natanmonteiro <84478590+natanmonteiro@users.noreply.github.com>
2021-09-22 11:38:22 +0100

    V3
    Booking System

--- src/algorithmsandconstructs/BookingSystem.java
+++ src/algorithmsandconstructs/BookingSystem.java
@@ -1,6 +1,6 @@
 /*
 @name: Natan Souza and Gabriel Jucá
-@date: 8th Sep 2021
+@date: 22th Sep 2021
 BookingSystem.java
  */
 package algorithmsandconstructs;
@@ -8,13 +8,32 @@ package algorithmsandconstructs;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class BookingSystem implements BookingSystemInterface{
+public class BookingSystem implements BookingSystemInterface {
 
+    //This is the Booking System to generate the bookings
     @Override
     public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        //Declaring variables
+        String cars = in.readLine();
+        String content = "";
+
+        RentACarInterface feed = new RentACar();
+        
+        //Loop to read line of feed
+        while (cars != null) {
+            System.out.println(cars);
+            content = in.readLine();
+            System.out.println(content);
+
+            Car item = new Car(cars, content);
+            feed.addItem(item);
+
+            cars = in.readLine();
+        }
+
+        return feed;
+
     }
-    
-    
-    
+
 }
